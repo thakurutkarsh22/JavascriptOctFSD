6939dc946a9fcd2049409a345c83e5b5083a0639
thkaurutkarsh22 <thkaurutkarsh22@gmail.com>
2023-04-05 16:28:56 +0530

    lecture 1 variables and introduction

--- InClass/Lecture1/script.js
+++ InClass/Lecture1/script.js
@@ -1,1 +1,58 @@
-console.log("hellow world")
+// console.log("hellow world")
+// java -> variable -> string, int, boolean , char, object -> ClassName
+
+// String name = "utkarsh";
+// int age = 12;
+
+// System.out.print(age, name) // 12 utkarsh --> wrong
+// System.out.print(age + " " + name) // 12  utkarsh ---> write
+
+
+// javascript  -> variable -> let, const 
+
+
+
+
+//  ----------- variables ---- ------ 
+
+
+// old JS  before ES-6 (ecma script version 6) 2015
+
+console.log("------------ BEFORE ES6 -----------")
+
+// var name = "utkarsh";
+// var age = 24;
+// var canVote = true;
+// var gender = 'M';
+
+// console.log(name, age, canVote, gender);
+
+// 2022 -> var wont use, ---> 2 new keywords -> let and cosnt 
+
+// variable (we can re-assign the value)
+
+let name = "utkarsh";
+let age = 24;
+let canVote = true;
+let gender = 'M'
+
+age = "26"
+
+console.log("hey there age", age)
+
+// constants (we can't re-assign)
+const lastName = "utkarsh const";
+// const age = 24;
+// const canVote = true;
+// const gender = 'M'
+
+lastName = "thakur"
+
+console.log(lastName);
+
+
+// HW -> why VAR are bad ??? 
+
+
+
+
