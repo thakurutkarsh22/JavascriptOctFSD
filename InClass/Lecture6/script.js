d606c62f1782ee9585b5f5672c03219a81d0b025
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2023-04-15 23:46:49 +0530

    first class functions debugging

---
+++ InClass/Lecture6/script.js
@@ -0,0 +1,162 @@
+///// ---------- - - - - - - -- - - - - - 
+
+
+
+// console.log(a)
+// var a = 12;
+
+// console.log(a)
+
+
+
+// console.log(b)
+// let b = undefined;
+
+
+
+
+// function sum(a ,b) {
+//     var c = a + b;
+//     return c;
+// }
+
+
+// const result = sum(1,2)
+
+// console.log(result);
+
+
+
+/*
+        FIRST CLASS FUNCTIONS: the languages which supports FIRST CLASS FUNCTIONS 
+        can treat their function like a VARIABLE.
+
+*/
+
+// Variable TREATMENT are as follows -> 
+
+// 1. we can declare variables.
+// 2. we can pass variables in functions as their arguments.
+// 3. we can return the variables from the function.
+
+// let num1 = 12;
+// let num2 = 13;
+
+// function sum(a , b) {
+//     //  a, and b are arguments. 
+//     let c = a + b;
+//     return c;
+// }
+
+// let result  = sum(num1, num2)
+
+
+
+// function calc(fn, val) {
+//     console.log(fn)
+//     console.log(val)
+// }
+
+// calc(sub, 2)
+
+// function sum(a,b) {
+//     return a+ b;
+// }
+
+// function sub(a,b) {
+//     return a-b
+// }
+
+
+
+
+// java -> 
+
+
+// public static void sum(int a, int b) {
+//     return a+ b;
+// }
+
+// int res = sum(1,3);
+
+
+// let num1 = 12;
+// let num2 = 13;
+
+// function abcd() {
+
+// }
+
+// function sum(a,b) {
+//     return a+b;
+// }
+
+// sum(num1, num2);
+
+// I am passing this VARIABLE as an argument of sum
+
+
+
+
+//  murcury -> weapons
+
+// earth -> life support
+
+// jupiter -> food rationis
+
+
+
+spaceShip(lifeSupport, 'utkarshasdasdasdasdas', 24)
+// space ships method 
+function spaceShip(func, num1, num2) {
+    // check for auth 
+    // log 
+
+    console.log(num1, num2);
+
+    if(num1 === "utkarsh") {
+        func()
+    } else {
+        console.log('hacking spaceship dectected')
+    }
+    
+
+
+}
+
+function weapons() {
+    console.log('weapons acrtivaged');
+}
+
+function lifeSupport() {
+    console.log('lifeSupport');
+}
+
+function food() {
+    console.log('food');
+}
+
+
+food()
+
+// call be reference vall byt value 
+
+food()
+
+food
+
+
+function sum(a,b) {
+    return a + b;
+}
+
+sum() // function call.
+
+sum
+
+
+
+// Question: what is the difference b/w sum() vs sum 
+
+
+
