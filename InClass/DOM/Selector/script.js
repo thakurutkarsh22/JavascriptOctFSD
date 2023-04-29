0db566c569aec868c5c00c72078c0deae0fe889f
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2023-04-29 22:17:13 +0530

    basic of dom and selector

---
+++ InClass/DOM/Selector/script.js
@@ -0,0 +1,37 @@
+/*
+    ... SELECTORS...
+*/
+
+
+// 1. getElementsByTagName
+
+// let paragraphTags = document.getElementsByTagName("p");
+
+// console.log(paragraphTags)
+
+
+// 2. getElementById
+
+// let paraElement = document.getElementById("paragraph");
+// console.log(paraElement);
+
+
+// 3. getElementsByClass
+
+// let containerElement = document.getElementsByClassName("container")
+// console.log(containerElement)
+
+
+// 4. querySelector -> it takes the CSS SELECTOR .
+
+const paragraphQueryele = document.querySelector("#paragraph");
+console.log(paragraphQueryele);
+
+
+
+
+
+
+
+
+
