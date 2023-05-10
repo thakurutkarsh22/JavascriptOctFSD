ca3e734bbb9e5415d0f24c88828f96d150848bc2
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2023-05-10 22:53:17 +0530

    closure intro and example.

--- InClass/Closures/script.js
+++ InClass/Closures/script.js
@@ -27,26 +27,86 @@
 
 
 /*
-    Closure -> function + its lexical Environment 
+    Closure -> function + its lexical Environment (surrounding env)
 */
 
-let a = 10; // global ? 
+// let a = 10; // global ? 
+
+// function foo() {
+    
+//     // nested function 
+//     function bar() {
+//         let c = 30 // local variable / bar Context 
+//         return a + b + c + abcd;
+//     }
+
+//     let b = 20 // foo varible  
+
+//     let abcd = 1000;
+    
+//     return bar;
+// }
+
+// const result = foo(); // bar ? 
+
+// console.log(result); // bar function ... 
+
+
+// const resultFinal = result(); //bar
+// console.log(resultFinal, "resultFinal") /// 
+
+
+
+
+
+
+
+
+
+
+// ----- Basic Example 
+
+
+// function abc() {
+
+//     // local environment ... 
+
+//     var c = 14
+//     console.log(a) // 
+//     console.log(c)
+// }
+
+// var a = 12;
+// abc()
+
+
+
+// / exercise
+
 function foo() {
+
+    let var1 = 10;
     
     function bar() {
-        let c = 30 // local variable / bar Context 
-        return a + b + c;
+        var1 = var1 + 1;
+        console.log("value of var 1 debug", var1)
     }
 
-    let b = 20 // foo varible  
     return bar;
 }
 
-const result = foo();
 
-console.log(result); // bar function ... 
+let resultOfExcesice = foo();
+resultOfExcesice(); // bar 11
+resultOfExcesice(); // bar 12
+resultOfExcesice(); // bar 13
+resultOfExcesice(); // bar 14
 
 
-const resultFinal = result();
-console.log(resultFinal, "resultFinal") /// 
+
+
+
+
+
+
 
