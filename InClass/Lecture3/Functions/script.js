d2a1834a54c627d91fce98f2a1d22c353cc9d4d2
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2023-05-10 23:47:02 +0530

    spread, rest, shallow copy

--- InClass/Lecture3/Functions/script.js
+++ InClass/Lecture3/Functions/script.js
@@ -81,29 +81,141 @@ function functionName(a, b) {
 
 // -------------- Function Expression --------------- 
 
-function sum(a, b) {
-    // do something here.
-    return a+b;
-}
+// function sum(a, b) {
+//     // do something here.
+//     return a+b;
+// }
 
-let sumExpression = function(a,b) {
-    if(typeof a == "number" && typeof b == "number") {
-        return a +b;
-    } else {
-        console.log("sahi number do")
-    }
+// let sumExpression = function(a,b) {
+//     if(typeof a == "number" && typeof b == "number") {
+//         return a +b;
+//     } else {
+//         console.log("sahi number do")
+//     }
     
-} 
+// } 
 
-let res1 = sum(1,2); // 3
+// let res1 = sum(1,2); // 3
 
-let res2 = sumExpression(1,2); // 3
+// let res2 = sumExpression(1,2); // 3
 
-console.log(res1, res2);
+// console.log(res1, res2);
 
 
 
 // TODO: Arrow functions ... (what is callbacks)
+
+
+
+//  -------------- REST OPERATOR ----------------------
+//  ... -> syntax
+ 
+function child(name, standard, rollNumber, ...otherInfo) {
+    console.log(name, standard, rollNumber )
+
+    console.log(otherInfo)
+}
+
+console.log("------------------ childs info ----------------");
+
+// child("akashay", "5th stand", 2, "Parent Father", "Parent Mother", "Delhi gali no 4", "Gaming pubg")
+
+
+// --------------------- default value of Argument -------------
+
+// function child(name, standard, rollNumber = 1) {
+//     console.log(name, standard, rollNumber )
+
+// }
+
+// child("akahsy", "5th class") // akahsy 5th class 1
+// child("akahsy", "5th class", 100)
+
+
+// --------------- Pass By Value and PAss By reference  -------------
+
+
+//  primitives are passed by value. (numbers, boolean);
+
+// let num1 = 1;
+// let num2 = 2;
+
+// function sum(a, b) {
+//     a = a + 10
+//     b = b + 10;
+
+//     console.log(a, b);
+
+//     return a + b
+// }
+
+// let resutSum = sum(num1,num2) // pass by value 
+
+// console.log(num1, num2);
+
+// let math = {
+//     marks: 100,
+//     grade: "A"
+// }
+
+// let science = {
+//     marks: 80,
+//     grade: "B"
+// }
+
+// function sum (obj1, obj2) {
+//     obj1.marks = 1
+//     obj2.marks = 2
+
+//     console.log(obj1.marks, obj2.marks, "obj marks") // ? 1 ->
+
+
+// }
+
+// sum(math, science)
+
+// console.log(math.marks, science.marks, " subject marks") // ?  2 -> 
+
+
+// ------- SPREAD OPERATOR 
+
+//  to create copies of any object....
+
+
+//- --- - - - - - - - --  DEEP CLONE and SHALLOW CLONE  -- - - - - - - - - - -
+
+let math = {
+    marks: 100,
+    grade: "A"
+}
+
+// Scanner scn = new Scanner () ;
+
+// SHALLOW COPY 
+let mathClone = { ...math }
+
+// let mathClone = { marks: 100, grade: "A" }
+
+let science = {
+    marks: 80,
+    grade: "B"
+}
+
+let scienceClone = { ...science }
+
+function sum (obj1, obj2) {
+    obj1.marks = 1
+    obj2.marks = 2
+
+    console.log(obj1.marks, obj2.marks, "obj marks") // ? 1 ->
+}
+
+sum(mathClone, scienceClone)
+
+console.log(math.marks, science.marks, " subject marks") // 2 ? 
+
+
+
 
 
 
