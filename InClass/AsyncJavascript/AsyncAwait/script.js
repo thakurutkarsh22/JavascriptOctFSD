150ad7d1326f51f8dafdd87937107bab4874266d
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2023-05-27 20:17:44 +0530

    async await intro

---
+++ InClass/AsyncJavascript/AsyncAwait/script.js
@@ -0,0 +1,73 @@
+// Async Await 
+
+// Await -> it looks like that the JS executor waits for the completion of the line.
+
+// function which is using async keyword 
+
+
+// async function returns a promise.
+
+
+// function abc() {
+//     return 1; 
+// }
+
+// const abcResult = abc()
+// console.log(abcResult, "abc result")
+
+// //  who's promise is this ? 
+// async function fun() {
+//     // await fetch("asdasdasdasd")
+
+//     return 1978
+// }
+
+// const funPromise = fun();
+
+// funPromise.then(data => {
+//     console.log("fun promise data", data)
+// })
+// console.log(funResult, "fun result ");
+
+
+
+
+/*
+    Implement Kanyes quote with async wait 
+*/
+
+
+async function fetchQuotes() {
+    const response = await fetch("https://api.kanye.rest/")
+    console.log(response)
+    const data = await response.json()
+    console.log(data); 
+
+    const {quote} = data;
+    console.log(quote)
+
+    addTotheDom(quote)
+}
+
+function addTotheDom(quote) {
+    const h1Element = document.createElement("h1");
+    h1Element.innerText = quote;
+
+    const body = document.body;
+    body.appendChild(h1Element)
+}
+
+
+fetchQuotes()
+
+
+
+
+
+
+// fetch 
+
+
+fetch()
+
+
--- InClass/AsyncJavascript/Promises/script.js
+++ InClass/AsyncJavascript/Promises/script.js
@@ -5,44 +5,147 @@
 // FETCH api to call the server... 
 
 
-console.log("hello ")
+// console.log("hello ")
 
 // const promiseOfQuote = 
 //     fetch('https://api.kanye.rest/')
 //         .then(responseObj => console.log(responseObj))
 
-const promiseOfQuote = 
-    fetch('https://api.kanye.rest12/').the // 10 sec 1 min 12 min resposne ? 
-        .then(responseObj => {
-            // something 
-            console.log(responseObj.status, " this is the status ")
-            return responseObj.json() //1 sec // line changes theresponseObj to Real data. 
-        })
-        .then(data => {
-            //  operations here ... 
-            const quote = data.quote;
-            // 1. create an element, edit that element
-            const h1Element = document.createElement('h1');
-            h1Element.innerText = quote;
-            // 2. Push that element inside the DOm tree. 
+// const promiseOfQuote = 
+//     fetch('https://api.kanye.rest/') // 10 sec 1 min 12 min resposne ? 
+//         .then(responseObj => {
+//             // something 
+//             console.log(responseObj.status, " this is the status ")
+//             return responseObj.json() //1 sec // line changes theresponseObj to Real data. 
+//         })
+//         .then(data => {
+//             //  operations here ... 
+//             const quote = data.quote;
+//             // 1. create an element, edit that element
+//             const h1Element = document.createElement('h1');
+//             h1Element.innerText = quote;
+//             // 2. Push that element inside the DOm tree. 
 
-            const body = document.body;
-            body.appendChild(h1Element)
+//             const body = document.body;
+//             body.appendChild(h1Element)
 
-            console.log(data, "real data")
-        }).catch(error => {
-            // handle that error 
-            // new beautiful page and i wl add it to the body 
-            console.log('Error is here')
-            console.log(error)
-        })
+//             console.log(data, "real data")
+//         }).catch(error => {
+//             // handle that error 
+//             // new beautiful page and i wl add it to the body 
+//             console.log('Error is here')
+//             console.log(error)
+//         })
 
-console.log(promiseOfQuote)
+// console.log(promiseOfQuote)
 
 
-console.log("bye")
+// console.log("bye")
 
 
 /* Question : take the quote and push it inside the dom(body).
 
 */
+
+
+
+/*
+        Behind the scenes 
+
+*/
+
+// console.log("hello")
+// fetch('https://api.kanye.reetfaks/')
+//         .then(response => {
+//             return response.json()
+//         }).then(data => {
+//             console.log("data")
+//         }).catch(error=> {
+//             console.log("erro")
+//         })
+
+// console.log("bye")
+
+
+/* 
+    Promise Chaining
+
+*/
+
+
+// console.log("hello")
+// fetch('https://api.kanye.restasaSas/')
+//         .then(response => {
+//             // response handler 
+//             // throw Error("aasdasdas")
+//             return response.json()
+//         }).then(data => {
+//             // data handler 
+//             console.log("data handler 0", data)
+
+//             return "1 output"
+//         }).then(data => {
+//             // number 1 handler 
+//             console.log("data handler 1", data)
+//         }).then(data => {
+//             // num 2 handler
+//         }).then(data => {
+//             // num3 handler 
+//         }).catch(erro => {
+//            console.log("errro block") 
+//         }).finally(() => {
+//             console.log("Https call is done  ")
+//         })
+
+// console.log("bye")
+
+
+
+/*
+    Error Handling 
+*/
+
+console.log("hello")
+fetch('https://api.kanye.rest/')
+        .then(response => {
+            // response handler 
+            throw Error("Error created by utkarsh ")
+            return response.json()
+        })
+        .then(data => {
+            // data handler 
+            console.log("data handler 0", data)
+
+            return "1 output"
+        }).catch(erro => {
+            console.log("error level 2", erro)
+        })
+        .then(data => {
+            // number 1 handler 
+            console.log("data handler 1", data)
+            return 1
+        }).then(data => {
+            // num 2 handler
+            throw Error("error in down level")
+            console.log("data handler 2", data)
+
+        }).then(data => {
+            // num3 handler 
+        }).catch(erro => {
+           console.log("errro block Level6", erro) 
+
+           // code for handling response handler
+
+        //    asdasda
+
+            // code for data handler 
+
+
+            // code for number 1 handler 
+        })
+
+console.log("bye")
+
+
+
+
