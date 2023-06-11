90631b8794ac5ca9915578682a365939bbcd2844
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2023-06-11 23:45:13 +0530

    more to pollyfills map, reduce

--- InClass/Pollyfills/script.js
+++ InClass/Pollyfills/script.js
@@ -5,45 +5,52 @@
 
 // Map 
 
-const resultMap = [89, 55, 68, 40].map( (item, index ) => {
-    return {
-        value: item,
-    }
-} )
+// const resultMap = [89, 55, 68, 40].map( (item, index, array ) => {
+//     return {
+//         index,
+//         value: item,
+//     }
+// } )
 
-console.log(resultMap, "map");
-
-
-// myMap
-
-/*
+// console.log(resultMap, "map");
 
 
-*/
+// // myMap
 
-Array.prototype.myMap = function( callback ) {
+// /*
+//     1. Go to every element 
+//         1.1 call the callback function
+//         1.2 whatever result you got from the call you need to save that.
 
-    let arr = this
+// */
 
-    const result = []; // this collects the returned val from the callback funciton.
+// Array.prototype.myMap = function( callback ) {
+//     // context -> this
 
-    for(let item of arr) {
-        const returnVal = callback(item)
-        result.push(returnVal)
-    }
+//     let arr = this
 
-    return result
-}
+//     const result = []; // this collects the returned val from the callback funciton.
 
-//this 
+//     for(let index = 0; index < arr.length ; index++){
+//         const item = arr[index];
+//         const returnVal = callback(item, index)
+//         result.push(returnVal)
+//     }
+//     return result
+// }
 
-const res = [123,123,123].myMap((item, index ) => {
-    return {
-        value: item,
-    }
-} )
+// //this 
 
-console.log(res, "my Map");
+// const callback = (item, index ) => {
+//     return {
+//         index,
+//         value: item,
+//     }
+// }
+
+// const res = [123,124,125].myMap( callback ) // callsite ?? binding ? implicit 
+
+// console.log(res, "my Map");
 
 
 // call 
@@ -53,6 +60,84 @@ console.log(res, "my Map");
 
 // bind 
 
+
+
+// Filter ----- >>
+
+// const callbackFilter = (item, index) => {
+//     return item > 7
+// }
+
+// const resultFilter = [1,2,3,4,5,6,7,8,9,10].filter( callbackFilter ) // [8 , 9 10]
+
+// console.log(resultFilter, "filter");
+
+
+// Array.prototype.myFilter = function( callback ) {
+
+//     let arr = this;
+
+//     let resultArr = [];
+
+//     for(let i = 0; i<arr.length; i++) {
+//         const item = arr[i];
+//         const res = callback(item,i );
+
+//         if(res) {
+//             resultArr.push(item);
+//         }
+//     }
+
+//     return resultArr;
+// }
+
+// const resultMyFilter = [1,2,3,4,5,6,7,8,9,10].myFilter( callbackFilter )
+// console.log(resultMyFilter, "result MyFilter");
+
+
+
+
+//  Reduce ------> 
+
+
+// const resultReduce = [2,3,4,5,6,7].reduce( (accumulator, currentVale ) => {
+//     return accumulator + currentVale
+// } )
+
+// console.log(resultReduce, "reduce");
+
+
+
+
+
+// Array.prototype.myReduce = function( callback, initialValue ) {
+//     let array = this;
+
+//     let accumulator = initialValue ? initialValue : 0 ;
+//     for (let index = 0; index < array.length; index++) {
+//         const item = array[index];
+//         accumulator = callback(accumulator ,item)
+//     }
+
+//     return accumulator;
+// }
+
+
+// const resultMyReduce = [2,3,4,5,6,7].myReduce( (accumulator, currentVale ) => {
+//     return accumulator + currentVale
+// } )
+
+// console.log(resultMyReduce, "my reduce");
+
+
+
+// Object
+
+// call apply bind,
+
+// Promises
+
+// setInteval();
 
 
 
